0409df35acd366fd1ee3f62f4bbe4da80c5d8e0f
alap33 <45278885+alap33@users.noreply.github.com>
2019-01-02 10:07:46 +0530

    Palindrome using StringBuffer class

--- P14.java
+++ P14.java
@@ -1,23 +1,29 @@
-//using string class
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package javaapplication14;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author student
+ */
 public class P14 {
     
-   
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
+        // TODO code application logic here
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the string");
         String s1=sc.nextLine();
-        String s2="";
-        int l=s1.length();
-        for(int i=(l-1);i>=0;i--)
-        {
-            s2=s2+s1.charAt(i);
-        }
-        if(s1.equals(s2)) 
+        StringBuffer s2=new StringBuffer(s1);
+        s2.reverse();
+        if(s1.equals(s2.toString())) 
         {
             System.out.println("Palindrme");
         }
@@ -28,5 +34,3 @@ public class P14 {
         }
 
     }
-    
-}
